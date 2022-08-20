a1d5aec3e4ed563c6139f0cfe5f0dd9b3a6cd0db
Prateek <prateekjain6342@gmail.com>
2022-08-20 13:31:43 +0530

    Added Upcoming Movies Heading Bar

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 import './Home.css';
 import Header from "../../common/header/Header";
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 const Home = () => {
     return (
-        <Header>
-
-        </Header>
+        <Fragment>
+        <Header />
+        <div className="upcoming-movies">
+            <div className="title-bar">
+                Upcoming Movies
+            </div>
+            
+        </div>
+        </Fragment>
     )
 }
 
